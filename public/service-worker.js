83aaa53bae3dbc0ee55b4eb3707158a419062d6f
teromanish53-ui <teromanish53@gmail.com>
2026-08-11 13:33:33 +0545

    Fix icon file paths in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'chalkboard-hangman-v1';
-const SHELL_FILES = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const SHELL_FILES = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
